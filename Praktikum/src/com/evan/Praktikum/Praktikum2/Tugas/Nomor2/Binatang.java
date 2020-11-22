f18a1fc30f90262ca5f4f9b694696b7986755440
Evan Devara <emmanuel.genesius@gmail.com>
2020-11-22 14:40:57 +0700

    Praktikum 2

---
+++ Praktikum/src/com/evan/Praktikum/Praktikum2/Tugas/Nomor2/Binatang.java
@@ -0,0 +1,16 @@
+package com.evan.Praktikum.Praktikum2.Tugas.Nomor2;
+
+public class Binatang {
+    // variabel
+    public String makan;
+    public String tidur;
+
+    public void makan(String nama) {
+        System.out.println(nama + " " + "Makan");
+    }
+
+    public void tidur(String nama) {
+        System.out.println(nama + " " + "Tidur");
+        System.out.println();
+    }
+}
